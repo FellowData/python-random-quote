12306586abd794dffb4f0fcf6c18a40ea19789fd
Hervé <rvleduc@gmail.com>
2020-09-04 22:18:05 +0200

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,9 +1,7 @@
 def main():
-<<<<<<< HEAD
+	
   print("Keep it logically awesome.")
-=======
-  #print("Keep it logically awesome.")
->>>>>>> ee7ed0d37475c660690bfe0f3c218dd3fef65629
+
 
   #f = open("quotes.txt")
   #quotes = f.readlines()
