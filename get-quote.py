bf2bc8a8aa3b3f385f069ee561954e507f03a318
Hervé <rvleduc@gmail.com>
2020-09-04 22:55:16 +0200

    Exercices complementaires

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,25 @@
 def primary():
-
+  import random
   #print("Keep it logically awesome.")
 
 
   f = open("quotes.txt")
   quotes = f.readlines()
+  #f.write ("Le boss est de retour - ", random.randint(1, 300))
   f.close()
 
-  print(quotes[13])
+  print ("il y a ", len(quotes),"citations")
+  #print(quotes[3].replace("\n", ""),quotes[4].replace("\n", ""))
+  compteur = 0
+  for x in quotes:
+  	compteur +=1
+  	print (compteur,"- ", x.replace("\n", ""))
+
+  f = open("quotes.txt", "a")
+  citation = "Le boss est de retour - " + str(random.randint(1, 300)) +"\n"
+  f.write (citation)
+  f.close()
+
 
 if __name__== "__main__":
   primary()
